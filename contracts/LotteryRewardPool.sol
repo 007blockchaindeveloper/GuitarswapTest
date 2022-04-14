4e9b13e3809e2086b8983a4470e55d7c381568ff
Jessica Nolan <dovasy0325@gmail.com>
2022-04-14 08:51:35 +0700

    code clean

--- contracts/LotteryRewardPool.sol
+++ contracts/LotteryRewardPool.sol
@@ -1,9 +1,5 @@
 pragma solidity 0.6.12;
 
-// import '@pancakeswap/pancake-swap-lib/contracts/token/BEP20/IBEP20.sol';
-// import '@pancakeswap/pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol';
-// import '@pancakeswap/pancake-swap-lib/contracts/access/Ownable.sol';
-
 import './MasterChef.sol';
 
 contract LotteryRewardPool is Ownable {
