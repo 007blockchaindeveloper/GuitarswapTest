04f3c9a334b492121d5fd99b7c2998ecda43adca
Jessica Nolan <dovasy0325@gmail.com>
2022-04-16 07:45:59 +0700

    config

--- truffle-config.js
+++ truffle-config.js
@@ -22,6 +22,11 @@ module.exports = {
   //}
   //
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"},
+
     testnet: {
       provider: () => new HDWalletProvider(MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
